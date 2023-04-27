90936f3b31e458400728964745bb768bb40c4052
hamza <hamza2140@outlook.com>
2023-04-27 10:34:13 +0200

    Dependencie problemen

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,117 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:flutter/material.dart';
-import 'vehicles.dart'; // importeer de klasse "Vehicle
+import 'package:firebase_auth/firebase_auth.dart';
+import 'vehicles.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Login',
-      home: LoginPage(),
+      home: LoginPage(auth: _auth),
     );
   }
 }
 
 class LoginPage extends StatefulWidget {
+  LoginPage({Key? key, required this.auth}) : super(key: key);
+
+  final FirebaseAuth auth;
+
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  String _username = "";
-  String _password = "";
-  String _errorMessage = "";
+  String? _username;
+  String? _password;
+  String? _errorMessage;
+  bool _isLoginForm = true;
+  final _formKey = GlobalKey<FormState>();
 
-  void _submit() {
-    if (_username == 'hamza' && _password == 'parkeer') {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => MyCustomWidget()),
-      );
-    } else {
-      setState(() {
-        _errorMessage = 'Ongeldige gebruikersnaam of wachtwoord';
-      });
+  void _submit() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      try {
+        UserCredential userCredential;
+        if (_isLoginForm) {
+          userCredential = await widget.auth.signInWithEmailAndPassword(
+              email: _username!, password: _password!);
+        } else {
+          userCredential = await widget.auth.createUserWithEmailAndPassword(
+              email: _username!, password: _password!);
+        }
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => MyCustomWidget()),
+        );
+      } on FirebaseAuthException catch (e) {
+        setState(() {
+          _errorMessage = 'Ongeldige gebruikersnaam of wachtwoord';
+        });
+      } catch (e) {
+        print(e);
+      }
     }
+  }
+
+  void _toggleFormMode() {
+    setState(() {
+      _isLoginForm = !_isLoginForm;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Login')),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'Gebruikersnaam'),
-              onChanged: (value) {
-                setState(() {
-                  _username = value;
-                });
-              },
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Gebruikersnaam'),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Gebruikersnaam is verplicht' : null,
+                  onSaved: (value) => _username = value,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Wachtwoord'),
+                  obscureText: true,
+                  validator: (value) =>
+                      value!.isEmpty ? 'Wachtwoord is verplicht' : null,
+                  onSaved: (value) => _password = value,
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: _submit,
+                  child: Text(_isLoginForm ? 'Inloggen' : 'Registreren'),
+                ),
+                TextButton(
+                  onPressed: _toggleFormMode,
+                  child: Text(_isLoginForm
+                      ? 'Nog geen account? Registreer nu'
+                      : 'Heb je al een account? Log in'),
+                ),
+                if (_errorMessage != null)
+                  Text(
+                    _errorMessage!,
+                    style: TextStyle(color: Colors.red),
+                  ),
+              ],
             ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Wachtwoord'),
-              obscureText: true,
-              onChanged: (value) {
-                setState(() {
-                  _password = value;
-                });
-              },
-            ),
-            ElevatedButton(
-              onPressed: _submit,
-              child: Text('Inloggen'),
-            ),
-            if (_errorMessage != null)
-              Text(
-                _errorMessage,
-                style: TextStyle(color: Colors.red),
-              ),
-          ],
+          ),
         ),
       ),
     );

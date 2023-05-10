d24e5cb16cd4da5ee13efc3f8dc9a611f557d7b7
hamza <hamza2140@outlook.com>
2023-05-10 13:38:35 +0200

    Map Indevelpment

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:latlong2/latlong.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:geocoding/geocoding.dart';
 //import 'vehicles.dart';
 
 void main() async {
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//login form
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -110,6 +112,17 @@ class _LoginPageState extends State<LoginPage> {
 }
 
 // open map form
+
+List<LatLng> parkingSpots = [
+  LatLng(51.260197, 4.402771),
+  LatLng(51.280197, 4.422771),
+  LatLng(51.265197, 4.412771),
+  LatLng(51.275197, 4.402771),
+  LatLng(51.255197, 4.432771),
+  LatLng(51.290197, 4.402771),
+  LatLng(51.240197, 4.422771),
+];
+
 class MyCustomWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -125,33 +138,98 @@ class MyCustomWidget extends StatelessWidget {
         ),
         MarkerLayer(
           markers: [
-            Marker(
-              point: LatLng(51.260197, 4.402771),
-              width: 50,
-              height: 50,
-              builder: (context) => Container(
-                child: Icon(
-                  Icons.location_on,
-                  color: Colors.red,
-                  size: 50.0,
+            for (var parkingSpot in parkingSpots)
+              Marker(
+                point: parkingSpot,
+                width: 50,
+                height: 50,
+                builder: (context) => GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ReservationFormPage(
+                          parkingSpot: parkingSpot,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    child: Icon(
+                      Icons.local_parking,
+                      color: Colors.green,
+                      size: 50.0,
+                    ),
+                  ),
                 ),
               ),
-            ),
-            Marker(
-              point: LatLng(51.280197, 4.422771),
-              width: 50,
-              height: 50,
-              builder: (context) => Container(
-                child: Icon(
-                  Icons.location_on,
-                  color: Colors.black,
-                  size: 50.0,
-                ),
-              ),
-            ),
           ],
         ),
       ],
     );
+  }
+}
+
+//reserveringspagina
+class ReservationFormPage extends StatelessWidget {
+  final LatLng parkingSpot;
+
+  ReservationFormPage({required this.parkingSpot});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reserveringsformulier'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Straatnaam: ${getStreetName()}',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'Tijdstip reserveren:',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Tijdstip',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                // Voeg hier de logica toe om de reservering te verwerken
+              },
+              child: Text('Reserveer'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<String> getStreetName() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+        parkingSpot.latitude,
+        parkingSpot.longitude,
+      );
+
+      if (placemarks.isNotEmpty) {
+        Placemark placemark = placemarks[0];
+        String streetName = placemark.street!;
+        return streetName;
+      }
+    } catch (e) {
+      print('Fout bij het ophalen van de straatnaam: $e');
+    }
+    return 'Onbekende straatnaam';
   }
 }
